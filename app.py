83f061e4d5a371769393755d3c3b1e6bfd348c86
santtuniskanen <santtuniska02@gmail.com>
2024-03-04 12:46:26 +0200

    added templates

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from models import Book
 
 app = Flask(__name__)
 
-# Create the database and table and insert some 
+# Create the database and table and insert some
 # test books into the database.
 if not os.path.isfile('books.db'):
     db.connect()
-    
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -18,9 +18,9 @@ def isValid(email):
     regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
     if re.fullmatch(regex, email):
         return True
-    else: 
+    else:
         return False
-        
+
 @app.route("/request", methods=['POST'])
 def postRequest():
     req_data = request.get_json()
@@ -39,19 +39,19 @@ def postRequest():
                 'res': f'Error! Book with title {title} is already in library!',
                 'status': '404'
             })
-            
+
     bk = Book(db.getNewId(), True, title, datetime.datetime.now())
     print('new book', bk.serialize())
     db.insert(bk)
     new_bks = [b.serialize() for b in db.view()]
     print('books in lib: ', new_bks)
-    
+
     return jsonify({
                 'res': bk.serialize(),
                 'status': '200',
                 'msg': 'Success creating a new book!'
     })
-    
+
 @app.route('/request', methods=['GET'])
 def getRequest():
     content_type = request.headers.get('Content-Type')
@@ -61,8 +61,107 @@ def getRequest():
         for b in bks:
             if b['id'] == int(json['id']):
                 return jsonify({
+                    # 'error': '',
                     'res': b,
-                    'status': 200,
-                    'msg': 'Success getting all books in library!'
+                    'status': '200',
+                    'msg': 'Success getting all books in library!👍😀'
                 })
-        return
+        return jsonify({
+            'error': f"Error! Book with id '{json['id']}' not found!",
+            'res': '',
+            'status': '404'
+        })
+    else:
+        return jsonify({
+                    # 'error': '',
+                    'res': bks,
+                    'status': '200',
+                    'msg': 'Success getting all books in library!👍😀',
+                    'no_of_books': len(bks)
+                })
+
+@app.route('/request/<id>', methods=['GET'])
+def getRequestId(id):
+    req_args = request.view_args
+    # print('req_args: ', req_args)
+    bks = [b.serialize() for b in db.view()]
+    if req_args:
+        for b in bks:
+            if b['id'] == int(req_args['id']):
+                return jsonify({
+                    # 'error': '',
+                    'res': b,
+                    'status': '200',
+                    'msg': 'Success getting book by ID!'
+                })
+        return jsonify({
+            'error': f"Error! Book with id '{req_args['id']}' was not found!",
+            'res': '',
+            'status': '404'
+        })
+    else:
+        return jsonify({
+                    # 'error': '',
+                    'res': bks,
+                    'status': '200',
+                    'msg': 'Success getting book by ID!👍😀',
+                    'no_of_books': len(bks)
+                })
+
+@app.route("/request", methods=['PUT'])
+def putRequest():
+    req_data = request.get_json()
+    availability = req_data['available']
+    title = req_data['title']
+    the_id = req_data['id']
+    bks = [b.serialize() for b in db.view()]
+    for b in bks:
+        if b['id'] == the_id:
+            bk = Book(
+                the_id,
+                availability,
+                title,
+                datetime.datetime.now()
+            )
+            print('new book: ', bk.serialize())
+            db.update(bk)
+            new_bks = [b.serialize() for b in db.view()]
+            print('books in lib: ', new_bks)
+            return jsonify({
+                # 'error': '',
+                'res': bk.serialize(),
+                'status': '200',
+                'msg': f'Success updating the book titled {title}!👍😀'
+            })
+    return jsonify({
+                # 'error': '',
+                'res': f'Error! Failed to update Book with title: {title}!',
+                'status': '404'
+            })
+    
+@app.route('/request/<id>', methods=['DELETE'])
+def deleteRequest(id):
+    req_args = request.view_args
+    print('req_args: ', req_args)
+    bks = [b.serialize() for b in db.view()]
+    if req_args:
+        for b in bks:
+            if b['id'] == int(req_args['id']):
+                db.delete(b['id'])
+                updated_bks = [b.serialize() for b in db.view()]
+                print('updated_bks: ', updated_bks)
+                return jsonify({
+                    'res': updated_bks,
+                    'status': '200',
+                    'msg': 'Success deleting book by ID!',
+                    'no_of_books': len(updated_bks)
+                })
+    else:
+        return jsonify({
+            'error': f"Error! No Book ID sent!",
+            'res': '',
+            'status': '404'
+        })
+
+if __name__ == '__main__':
+    app.run()
